1c17703682bcaf6800486277402a9347d25a6297
Mateus Alves <m.alvesealves@gmail.com>
2025-01-07 16:20:29 -0300

    initial backend commit

---
+++ e-comm-backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
+const categoryRoutes = require("./routes/category");
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(port, () => {
+  console.log("Server running on port:", port);
+});
+
+app.use("/category", categoryRoutes);
+
+async function connectDb() {
+  await mongoose.connect("mongodb://localhost:27017", {
+    dbName:"e-commerce-store-db"
+  })
+}
+
+connectDb().catch((err) => {
+  console.log(err);
+})
---
+++ e-comm-backend/routes/category.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const {
+  addCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../handlers/category-handler");
+
+router.post("", async (req, res) => {
+  let model = req.body;
+  let result = await addCategory(model);
+  res.send(result);
+});
+
+router.put("/:id", async (req, res) => {
+  let model = req.body;
+  let id = req.params["id"];
+  await updateCategory(id, model);
+  res.send({ message: "Registro atualizado" });
+});
+
+router.delete("/:id", async (req, res) => {
+  let id = req.params["id"];
+  await deleteCategory(id);
+  res.send({ message: "Registro excluído" });
+});
+
+module.exports = router;
